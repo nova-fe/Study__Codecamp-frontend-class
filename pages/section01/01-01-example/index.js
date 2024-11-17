5c596a302191115b8732d2e35b2338452ff20edb
Nova <heetask@gmail.com>
2024-11-17 13:44:14 +0900

    [학습] 01-01-example

---
+++ pages/section01/01-01-example/index.js
@@ -0,0 +1,12 @@
+export default function myPage() {
+  // * javascript 작성하는 공간
+  const num = 123;
+
+  return (
+    // * html 작성하는 공간
+    <div>
+      <div>Nova's Page</div>
+      <button>Click!!!</button>
+    </div>
+  )
+}
